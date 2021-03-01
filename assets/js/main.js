110c2a34607cbb697ebd7433167eeb257cd1e45b
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2021-03-01 14:58:50 -0800

    Update resume

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function($) {
+(function ($) {
 
 	var $window = $(window),
 		$body = $('body'),
@@ -14,146 +14,146 @@
 		$wrapper = $('#wrapper');
 
 	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '1025px',  '1280px' ],
-			medium:   [ '737px',   '1024px' ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ null,      '480px'  ],
-		});
+	breakpoints({
+		xlarge: ['1281px', '1680px'],
+		large: ['1025px', '1280px'],
+		medium: ['737px', '1024px'],
+		small: ['481px', '736px'],
+		xsmall: [null, '480px'],
+	});
 
 	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+	$window.on('load', function () {
+		window.setTimeout(function () {
+			$body.removeClass('is-preload');
+		}, 100);
+	});
 
 	// Tweaks/fixes.
 
-		// Polyfill: Object fit.
-			if (!browser.canUse('object-fit')) {
+	// Polyfill: Object fit.
+	if (!browser.canUse('object-fit')) {
 
-				$('.image[data-position]').each(function() {
+		$('.image[data-position]').each(function () {
 
-					var $this = $(this),
-						$img = $this.children('img');
+			var $this = $(this),
+				$img = $this.children('img');
 
-					// Apply img as background.
-						$this
-							.css('background-image', 'url("' + $img.attr('src') + '")')
-							.css('background-position', $this.data('position'))
-							.css('background-size', 'cover')
-							.css('background-repeat', 'no-repeat');
+			// Apply img as background.
+			$this
+				.css('background-image', 'url("' + $img.attr('src') + '")')
+				.css('background-position', $this.data('position'))
+				.css('background-size', 'cover')
+				.css('background-repeat', 'no-repeat');
 
-					// Hide img.
-						$img
-							.css('opacity', '0');
+			// Hide img.
+			$img
+				.css('opacity', '0');
 
-				});
+		});
 
-			}
+	}
 
 	// Header Panel.
 
-		// Nav.
-			var $nav_a = $nav.find('a');
+	// Nav.
+	var $nav_a = $nav.find('a');
 
-			$nav_a
-				.addClass('scrolly')
-				.on('click', function() {
+	$nav_a
+		.addClass('scrolly')
+		.on('click', function () {
 
-					var $this = $(this);
+			var $this = $(this);
 
-					// External link? Bail.
-						if ($this.attr('href').charAt(0) != '#')
-							return;
+			// External link? Bail.
+			if ($this.attr('href').charAt(0) != '#')
+				return;
 
-					// Deactivate all links.
+			// Deactivate all links.
+			$nav_a.removeClass('active');
+
+			// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+			$this
+				.addClass('active')
+				.addClass('active-locked');
+
+		})
+		.each(function () {
+
+			var $this = $(this),
+				id = $this.attr('href'),
+				$section = $(id);
+
+			// No section for this link? Bail.
+			if ($section.length < 1)
+				return;
+
+			// Scrollex.
+			$section.scrollex({
+				mode: 'middle',
+				top: '5vh',
+				bottom: '5vh',
+				initialize: function () {
+
+					// Deactivate section.
+					$section.addClass('inactive');
+
+				},
+				enter: function () {
+
+					// Activate section.
+					$section.removeClass('inactive');
+
+					// No locked links? Deactivate all links and activate this section's one.
+					if ($nav_a.filter('.active-locked').length == 0) {
+
 						$nav_a.removeClass('active');
+						$this.addClass('active');
 
-					// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
-						$this
-							.addClass('active')
-							.addClass('active-locked');
+					}
 
-				})
-				.each(function() {
+					// Otherwise, if this section's link is the one that's locked, unlock it.
+					else if ($this.hasClass('active-locked'))
+						$this.removeClass('active-locked');
 
-					var	$this = $(this),
-						id = $this.attr('href'),
-						$section = $(id);
+				}
+			});
 
-					// No section for this link? Bail.
-						if ($section.length < 1)
-							return;
+		});
 
-					// Scrollex.
-						$section.scrollex({
-							mode: 'middle',
-							top: '5vh',
-							bottom: '5vh',
-							initialize: function() {
+	// Title Bar.
+	$titleBar = $(
+		'<div id="titleBar">' +
+		'<a href="#header" class="toggle"></a>' +
+		'<span class="title">' + $('#logo').html() + '</span>' +
+		'</div>'
+	)
+		.appendTo($body);
 
-								// Deactivate section.
-									$section.addClass('inactive');
-
-							},
-							enter: function() {
-
-								// Activate section.
-									$section.removeClass('inactive');
-
-								// No locked links? Deactivate all links and activate this section's one.
-									if ($nav_a.filter('.active-locked').length == 0) {
-
-										$nav_a.removeClass('active');
-										$this.addClass('active');
-
-									}
-
-								// Otherwise, if this section's link is the one that's locked, unlock it.
-									else if ($this.hasClass('active-locked'))
-										$this.removeClass('active-locked');
-
-							}
-						});
-
-				});
-
-		// Title Bar.
-			$titleBar = $(
-				'<div id="titleBar">' +
-					'<a href="#header" class="toggle"></a>' +
-					'<span class="title">' + $('#logo').html() + '</span>' +
-				'</div>'
-			)
-				.appendTo($body);
-
-		// Panel.
-			$header
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'right',
-					target: $body,
-					visibleClass: 'header-visible'
-				});
+	// Panel.
+	$header
+		.panel({
+			delay: 500,
+			hideOnClick: true,
+			hideOnSwipe: true,
+			resetScroll: true,
+			resetForms: true,
+			side: 'right',
+			target: $body,
+			visibleClass: 'header-visible'
+		});
 
 	// Scrolly.
-		$('.scrolly').scrolly({
-			speed: 1000,
-			offset: function() {
+	$('.scrolly').scrolly({
+		speed: 1000,
+		offset: function () {
 
-				if (breakpoints.active('<=medium'))
-					return $titleBar.height();
+			if (breakpoints.active('<=medium'))
+				return $titleBar.height();
 
-				return 0;
+			return 0;
 
-			}
-		});
+		}
+	});
 
 })(jQuery);
